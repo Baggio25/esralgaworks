7c59a469c94a389f26226e27a08f74c30d1be20d
rodrigo.baggio.si@gmail.com <Baggio25>
2024-02-06 07:52:34 -0300

    4.23. Modelando e implementando a inclusão de recursos com POST

--- src/main/java/com/baggio/esralgaworks/api/controller/CozinhaController.java
+++ src/main/java/com/baggio/esralgaworks/api/controller/CozinhaController.java
@@ -1,13 +1,17 @@
 package com.baggio.esralgaworks.api.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.baggio.esralgaworks.domain.model.Cozinha;
 import com.baggio.esralgaworks.domain.repository.CozinhaRepository;
@@ -29,6 +33,15 @@ public class CozinhaController {
   public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
     Cozinha cozinha = cozinhaRepository.buscar(id);
     return ResponseEntity.ok(cozinha);
+  }
+
+  @PostMapping
+  public ResponseEntity<Cozinha> salvar(@RequestBody Cozinha cozinha) {
+    cozinha = cozinhaRepository.salvar(cozinha);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                    .buildAndExpand(cozinha.getId()).toUri();
+
+    return ResponseEntity.created(uri).body(cozinha);
   }
 
 }
