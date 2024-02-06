c070607ddcfd4313ce9fb862b52502b6a33232ff
rodrigo.baggio.si@gmail.com <Baggio25>
2024-02-06 08:52:36 -0300

    4.26. Modelando e implementando a exclusão de recursos com DELETE

--- src/main/java/com/baggio/esralgaworks/api/controller/CozinhaController.java
+++ src/main/java/com/baggio/esralgaworks/api/controller/CozinhaController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,6 +55,14 @@ public class CozinhaController {
     cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
    
     return ResponseEntity.ok(cozinhaAtual);
+  }
+
+  @DeleteMapping(value = "/{id}")
+  public ResponseEntity<Void> excluir(@PathVariable Long id) {
+    Cozinha cozinhaAtual = cozinhaRepository.buscar(id);
+    cozinhaRepository.remover(cozinhaAtual);
+   
+    return ResponseEntity.noContent().build();
   }
 
 
