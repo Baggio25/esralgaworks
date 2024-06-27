c2e157d643e125b3b65a0b4d339737691b6e15e0
rodrigo.baggio.si@gmail.com <Baggio25>
2024-06-27 13:39:21 -0300

    8.20. Customizando exception handlers de ResponseEntityExceptionHandler

--- src/main/java/com/baggio/esralgaworks/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/baggio/esralgaworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package com.baggio.esralgaworks.api.exceptionhandler;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -13,66 +14,78 @@ import com.baggio.esralgaworks.domain.exception.EntidadeNaoEncontradaException;
 import com.baggio.esralgaworks.domain.exception.NegocioException;
 
 @ControllerAdvice
-public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> handleEntidadeNaoEncontradaException(
 			EntidadeNaoEncontradaException e, WebRequest request) {
-		
+
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
 		String detail = e.getMessage();
-		
-		Problem problem = createProblemBuilder(status, problemType, detail).build();		
-		
+
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+
 		return handleExceptionInternal(e, problem, new HttpHeaders(),
 				status, request);
 	}
-	
+
 	@ExceptionHandler(NegocioException.class)
 	public ResponseEntity<?> handleNegocioException(
 			NegocioException e, WebRequest request) {
-		
+
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		ProblemType problemType = ProblemType.ERRO_NEGOCIO;
 		String detail = e.getMessage();
-		
+
 		Problem problem = createProblemBuilder(status, problemType, detail).build();
-		
+
 		return handleExceptionInternal(e, problem, new HttpHeaders(),
 				status, request);
 	}
-	
+
 	@ExceptionHandler(EntidadeEmUsoException.class)
 	public ResponseEntity<?> handleEntidadeEmUsoException(
-			EntidadeEmUsoException e, WebRequest request){	
-		
+			EntidadeEmUsoException e, WebRequest request) {
+
 		HttpStatus status = HttpStatus.CONFLICT;
 		ProblemType problemType = ProblemType.ENTIDADE_EM_USO;
 		String detail = e.getMessage();
-		
+
 		Problem problem = createProblemBuilder(status, problemType, detail).build();
-		
+
 		return handleExceptionInternal(e, problem, new HttpHeaders(),
 				status, request);
 	}
-	
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException e, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+
+		ProblemType problemType = ProblemType.MENSAGEM_INCROMPREENSIVEL;
+		String detail = "O corpo da requisição está inválido. Verifique o erro de sintaxe";
+
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+		return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
+	}
+
 	@Override
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
-		
-		if(body == null) {
+
+		if (body == null) {
 			body = Problem.builder()
 					.title(status.getReasonPhrase())
 					.status(status.value())
-					.build();	
-		}else if(body instanceof String) {
+					.build();
+		} else if (body instanceof String) {
 			body = Problem.builder()
 					.title((String) body)
 					.status(status.value())
-					.build();	
+					.build();
 		}
-		
+
 		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
 
