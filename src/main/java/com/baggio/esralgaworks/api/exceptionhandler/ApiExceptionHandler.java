c83eb4fd29bca52cdee1b6b199d99960b59254f1
rodrigo.baggio.si@gmail.com <Baggio25>
2024-07-02 08:02:55 -0300

    9.4. Estendendo o Problem Details para adicionar as propriedades com constraints violadas

--- src/main/java/com/baggio/esralgaworks/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/baggio/esralgaworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.baggio.esralgaworks.api.exceptionhandler;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -147,11 +151,21 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
+		
 		ProblemType problemType = ProblemType.DADOS_INVALIDOS;
 		String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
+		BindingResult bindingResult = ex.getBindingResult();
+		
+		List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
+						.map(fieldError -> Problem.Field.builder()
+										.name(fieldError.getField())
+										.userMessage(fieldError.getDefaultMessage())
+										.build())
+						.collect(Collectors.toList());
 
 		Problem problem = createProblemBuilder(status, problemType, detail)
 				.userMessage(detail)
+				.fields(problemFields)
 				.build();
 
 		return handleExceptionInternal(ex, problem, headers, status, request);
--- src/main/java/com/baggio/esralgaworks/api/exceptionhandler/Problem.java
+++ src/main/java/com/baggio/esralgaworks/api/exceptionhandler/Problem.java
@@ -1,5 +1,7 @@
 package com.baggio.esralgaworks.api.exceptionhandler;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -20,5 +22,14 @@ public class Problem {
 	private String title;
 	private String detail;
 	private String userMessage;
+
+	private List<Field> fields;
 	
+	@Getter
+	@Builder
+	public static class Field {
+		private String name;
+		private String userMessage;
+	}
+
 }
