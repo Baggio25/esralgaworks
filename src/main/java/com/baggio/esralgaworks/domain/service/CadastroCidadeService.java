c31f2365323fc3f15e1688a4c44947fa64c90b39
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2024-07-03 17:29:46 -0300

    [200~11.1. Analisando e definindo melhor o escopo das transações

--- src/main/java/com/baggio/esralgaworks/domain/service/CadastroCidadeService.java
+++ src/main/java/com/baggio/esralgaworks/domain/service/CadastroCidadeService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.baggio.esralgaworks.domain.exception.CidadeNaoEncontradaException;
 import com.baggio.esralgaworks.domain.exception.EntidadeEmUsoException;
@@ -22,6 +23,7 @@ public class CadastroCidadeService {
 	@Autowired
 	private CadastroEstadoService estadoService;
 
+	@Transactional
 	public Cidade salvar(Cidade cidade) {
 		Long estadoId = cidade.getEstado().getId();
 		Estado estado = estadoService.buscarOuFalhar(estadoId);
@@ -30,6 +32,7 @@ public class CadastroCidadeService {
 		return cidadeRepository.save(cidade);
 	}
 
+	@Transactional
 	public void excluir(Long id) {
 		try {
 			cidadeRepository.deleteById(id);
--- src/main/java/com/baggio/esralgaworks/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/baggio/esralgaworks/domain/service/CadastroRestauranteService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.baggio.esralgaworks.domain.exception.CozinhaNaoEncontradaException;
 import com.baggio.esralgaworks.domain.exception.EntidadeEmUsoException;
@@ -24,6 +25,7 @@ public class CadastroRestauranteService {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 
+	@Transactional
 	public Restaurante salvar(Restaurante restaurante) {
 		Long cozinhaId = restaurante.getCozinha().getId();
 		Cozinha cozinha = cozinhaRepository.findById(cozinhaId)
@@ -33,6 +35,7 @@ public class CadastroRestauranteService {
 		return restauranteRepository.save(restaurante);
 	}
 
+	@Transactional
 	public void excluir(Long id) {
 		try {
 			restauranteRepository.deleteById(id);
